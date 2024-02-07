03eb49ec2920d9577b91ab87ed8196a5feeb0a33
hasidew <hasinidewmini075@gmail.com>
2024-02-08 00:48:35 +0530

    update JS for book borrower

--- Feature_Book Borrow.php
+++ Feature_Book Borrow.php
@@ -96,6 +96,11 @@
     </table>
 </div>
 
+<!-- Bootstrap JS and Popper.js (required for Bootstrap components) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 <!-- JavaScript -->
 
 <script>
