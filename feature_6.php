4ba3e6ee3fd5cf3635745060ac43cbced5739f47
ChamodUthpala <chamoduthpala1@gmail.com>
2024-02-07 11:03:14 +0530

    Create form
    Create text inputs and buttons

--- feature_6.php
+++ feature_6.php
@@ -3,9 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Library Management System</title>
 </head>
 <body>
-    
+
+<div class="container">
+    <h2>Assign Fine</h2>
+    <form id="assignFineForm" method="post">
+    <div class="form-group">
+            <label for="fineID">Fine ID:</label>
+            <input type="text" class="form-control" id="fineID" name="fineID" required>
+        </div>
+        <div class="form-group">
+            <label for="memberID">Member ID:</label>
+            <input type="text" class="form-control" id="memberID" name="memberID" required>
+        </div>
+        <div class="form-group">
+            <label for="bookID">Book ID:</label>
+            <input type="text" class="form-control" id="bookID" name="bookID" required>
+        </div>
+        <div class="form-group">
+            <label for="fineAmount">Fine Amount (LKR):</label>
+            <input type="number" class="form-control" id="fineAmount" name="fineAmount" required>
+           
+        </div>
+        <button type="submit" class="btn btn-custom">Assign Fine</button>
+        <button type="reset" class="btn btn-custom" >Clear Data</button>
+    </form>
+</div>
+
 </body>
 </html>
