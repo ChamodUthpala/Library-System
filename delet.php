6d2fd784eb7fa9516752d668435f536d57cae293
ChamodUthpala <159267886+damithaDananjaya17@users.noreply.github.com>
2024-02-08 13:33:38 +0530

    Create delet file for create delete function

---
+++ delet.php
@@ -0,0 +1,18 @@
+<?php
+include 'db_config.php';
+
+if (isset($_GET['delete_category_id'])) {
+    $deleteCategoryId = $_GET['delete_category_id'];
+
+    // Delete category record
+    $deleteQuery = "DELETE FROM bookcategory WHERE category_id = '$deleteCategoryId'";
+
+    $result = $conn->query($deleteQuery);
+
+    if (!$result) {
+        die('Error: ' . $conn->error);
+    }
+}
+
+$conn->close();
+header('Location: index.php');
