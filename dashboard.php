4296f2ed623aba758db52fc0ceee14aa7bcb3a9d
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-12 20:07:48 +0530

    dashboard html is added

---
+++ dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System Dashboard</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    </head>
+<body>
+<div class="container1">
+    <div class="container">
+        <h1 class="text-center">Library Management System </h1></br></br>
+
+        <div class="btn-container">
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='login_registration.php'">Login and User Registration</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='book_registration.php'">Book Registration</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='book_category_registration.php'">Book Category Registration</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='library_member_registration.php'">Library Member Registration</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='book_borrow_details.php'">Book Borrow Details</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='feature6.php'">Assign Fine for a User</button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
