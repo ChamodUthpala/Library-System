634773bb8cf630dde1e2979d727c676d1651f83f
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-13 22:26:28 +0530

    login page css is updated

--- login.php
+++ login.php
@@ -49,6 +49,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <style>
         body {
             font-family: Arial, sans-serif;
