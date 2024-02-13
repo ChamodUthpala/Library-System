f076b68dfc5a08936a1706553a95cd990e73bb85
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-13 13:45:20 +0530

    login page php is updated

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "library_test";
+$database = "library_system";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
