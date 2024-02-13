c55d26b6a00920820bab392f790e2d06d988bea1
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-13 22:23:38 +0530

    login reg css is updated

--- login-reg.php
+++ login-reg.php
@@ -117,6 +117,7 @@ $conn->close();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Staff Registration</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <style>
         body {
             font-family: Arial, sans-serif;
