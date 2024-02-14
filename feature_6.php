9e5403cc74152ee429b3396944f6ebd364fc3363
ChamodUthpala <chamoduthpala1@gmail.com>
2024-02-14 18:36:26 +0530

    Change background image

--- feature_6.php
+++ feature_6.php
@@ -10,7 +10,7 @@
 
     <style>
         body {
-            background-image: url('library.jpg');
+            background-image: url('image1.jfif');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
