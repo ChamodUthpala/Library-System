c713068cf95dc60a5db8ab7f0b0f2be0eed46f5c
IshiniSamarasinghe <samaras-ct19001@stu.kln.ac.lk>
2024-02-14 13:02:45 +0530

    dashboard html is updated

--- dashboard.php
+++ dashboard.php
@@ -53,7 +53,7 @@
         <h1 class="text-center">Library Management System </h1></br></br>
 
         <div class="btn-container">
-            <button class="btn btn-primary btn-lg" onclick="window.location.href='login_registration.php'">Login and User Registration</button></br>
+            <button class="btn btn-primary btn-lg" onclick="window.location.href='login-reg.php'">Login and User Registration</button></br>
             <button class="btn btn-primary btn-lg" onclick="window.location.href='book_registration.php'">Book Registration</button></br>
             <button class="btn btn-primary btn-lg" onclick="window.location.href='book_category_registration.php'">Book Category Registration</button></br>
             <button class="btn btn-primary btn-lg" onclick="window.location.href='library_member_registration.php'">Library Member Registration</button></br>
